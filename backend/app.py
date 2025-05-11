eecb3afc285d63223d9bd8614a9ea4fa8c9117d3
hairil01 <muhammadhairil0981@gmail.com>
2025-05-11 14:59:48 +0800

    Remove debug=True

--- backend/app.py
+++ backend/app.py
@@ -24,4 +24,4 @@ def update_sensor_data():
     return jsonify({"error": "Invalid JSON"}), 400
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
